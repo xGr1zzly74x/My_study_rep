ab5b9d68331253b766475c777a26c4e74b22704b
xGr1zzly74x <ivanshabalin174@gmail.com>
2023-07-24 20:16:46 +0500

    1
    1

--- HomeWork_1/fetch_on_browser.js
+++ HomeWork_1/fetch_on_browser.js
@@ -1,4 +1,8 @@
-  function select(){
+const arr_pok10  = new Array()
+const arr_pok2_5 = new Array()
+let uniqDate     = new Array()
+
+document.getElementById("button_select").addEventListener("click", function(event){
   const place_name = document.getElementById("city").value
   const API_KEY_YANDEX = "85eaff1b-ef9e-4c11-89bc-ca01d1ae43de"
   const API_URL_GEO_DATA = `https://geocode-maps.yandex.ru/1.x/?apikey=${API_KEY_YANDEX}&geocode=${place_name}&format=json`
@@ -22,7 +26,7 @@
         // .then(resp_meteo => console.log(resp_meteo.hourly.pm2_5))
 
         .then((resp_meteo) => {
-          document.querySelector(".div_table_pol").style.display= "block"
+          document.querySelector(".div_table_pol").style.display = "block"
           document.querySelector(".div_table_pol").innerHTML = `<table class ="table_pol"></table>`
 
           let head = document.createElement("thead")
@@ -33,19 +37,16 @@
 
           document.querySelector(".table_pol").appendChild(head)
           let length = resp_meteo.hourly.time.length //Считаем, что массивы одинаковой длины
-          let date_arr = resp_meteo.hourly.time.map(item => {
-            return item.slice(0,10)
+          let date_arr = resp_meteo.hourly.time.map((item) => {
+            return item.slice(0, 10)
           })
-          let uniqDate = new Set(date_arr);
-          //console.log(uniqDate) массив значений x (даты)
+
+          uniqDate = new Set(date_arr)
 
           let now_dat
           let prev_dat
           let sum_pm10
           let sum_pm2_5
-
-          const arr_pok10 = new Array()
-          const arr_pok2_5 = new Array()
 
           for (let i = 0; i < length; i += 1) {
             let row = document.createElement("tr")
@@ -61,34 +62,30 @@
 
             now_dat = m_time_date[0]
 
-            if(prev_dat == null) {
+            if (prev_dat == null) {
               sum_pm10 = resp_meteo.hourly.pm10[i]
               sum_pm2_5 = resp_meteo.hourly.pm2_5[i]
-            }
-            else{
-              if(now_dat == prev_dat) {
+
+            } else {
+              if (now_dat == prev_dat) {
                 sum_pm10 += resp_meteo.hourly.pm10[i]
                 sum_pm2_5 += resp_meteo.hourly.pm2_5[i]
-              }
-              else{
-                arr_pok10.push(sum_pm10/24)
-                arr_pok2_5.push(sum_pm2_5/24)
+                
+              } else {
+                arr_pok10.push(sum_pm10 / 24)
+                arr_pok2_5.push(sum_pm2_5 / 24)
                 sum_pm10 = resp_meteo.hourly.pm10[i]
                 sum_pm2_5 = resp_meteo.hourly.pm2_5[i]
               }
             }
 
-            if (i == length){
-              arr_pok10.push(sum_pm10/24)
-              arr_pok2_5.push(sum_pm2_5/24)
+            if (i == length) {
+              arr_pok10.push(sum_pm10 / 24)
+              arr_pok2_5.push(sum_pm2_5 / 24)
             }
             prev_dat = m_time_date[0]
           }
-          //console.log(arr_pok10) массив значений y (показатели загрязнения)
-          //console.log(arr_pok2_5) массив значений y (показатели загрязнения)
-
         })
       return resp_city
     })
-  }
-//})
+})
