c88bdff21ea348ea6ed0ded4b87e6466fa1b12fc
Шабалин Иван Алексеевич <ShabalinIA@int.mechel.corp>
2023-09-20 13:10:41 +0500

    1
    1

--- HomeWork_5/events-world-app-master/src/index.tsx
+++ HomeWork_5/events-world-app-master/src/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { store } from "../src/store";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { store } from "../src/store"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
+import { persistStore } from "redux-persist"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
-);
+)
 
 let persistor = persistStore(store);
 
@@ -25,9 +25,9 @@ root.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
