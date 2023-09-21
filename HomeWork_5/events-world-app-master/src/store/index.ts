017bb45d28b7e6a3ad58cc50088565cf4d60c74f
Шабалин Иван Алексеевич <ShabalinIA@int.mechel.corp>
2023-09-21 10:59:47 +0500

    1
    1

--- HomeWork_5/events-world-app-master/src/store/index.ts
+++ HomeWork_5/events-world-app-master/src/store/index.ts
@@ -22,5 +22,5 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
 })
 
-//export type AppDispatch = typeof store.dispatch
-//export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
