28608d5424c6b688b7dfcae3348d27d7235015f9
Иван Шабалин <139092751+xGr1zzly74x@users.noreply.github.com>
2023-11-18 22:06:39 +0500

    1
    1

---
+++ Att_3/events-world-app-master/cypress/e2e/test_login.cy.js
@@ -0,0 +1,11 @@
+describe("Att_3", () => {
+  it("test sign in", () => {
+    cy.visit("http://localhost:3000/")
+    cy.get('#login_ex').type("login")
+    cy.get('#password_ex').type("login")
+    cy.contains("Вход").click()
+    cy.contains("Вы успешно авторизованы на сервисе!")
+    cy.contains("Выход").click()
+    cy.contains("Войдите в свою учетную запись")
+  })
+})
